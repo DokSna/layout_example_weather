107bae109f0f32e1097de07e64b0cd6403bdcee2
Artem <31787423+DokSna@users.noreply.github.com>
2022-04-17 13:00:40 +0300

    добавлена картинка

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Weather", style: TextStyle(color: Colors.black87)),
+          title: const Text(
+            "Weather",
+            style: TextStyle(color: Colors.black87),
+          ),
           centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          iconTheme: const IconThemeData(color: Colors.black54),
+          brightness: Brightness.light,
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.settings),
+            ),
+          ],
         ),
+        body: _buildBody(),
       ),
     );
   }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(children: <Widget>[
+      _headerImage(),
+    ]),
+  );
+}
+
+Image _headerImage() {
+  return const Image(
+    image: AssetImage('assets/images/sun2.jpg'),
+    fit: BoxFit.cover,
+  );
 }
